ddcbacacbfbc334b5868b6f5ddcdfaff9dc8770d
Andrew-R-Y <rakitinandrei@gmail.com>
2024-01-01 22:03:13 +0300

    feat: add solution of shiftArray task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -603,8 +603,16 @@ function propagateItemsByPositionIndex(arr) {
  *    shiftArray(['a', 'b', 'c', 'd'], -1) => ['b', 'c', 'd', 'a']
  *    shiftArray([10, 20, 30, 40, 50], -3) => [40, 50, 10, 20, 30]
  */
-function shiftArray(/* arr, n */) {
-  throw new Error('Not implemented');
+function shiftArray(arr, n) {
+  let result;
+  if (n > 0) {
+    const removedArr = arr.splice(-n);
+    result = removedArr.concat(arr);
+  } else {
+    const removedArr = arr.splice(0, -n);
+    result = arr.concat(removedArr);
+  }
+  return result;
 }
 
 /**
